fea96fd989bfc004603ba2a6dbf6fcdcf97bb582
Matthew Weitzel <mweitzel@teededuh.local>
2013-10-13 13:11:36 -0700

    added 'state' getter to retreive url hash without #

--- reclap.js
+++ reclap.js
@@ -1,5 +1,12 @@
 reclap = (function(){
   var public = {}
-  
+
+  public.getState = function(){
+    var state = window.location.hash
+    if( state[0] == '#' )
+      state = state.slice( 1 );
+    return state
+  }
+
   return public
 })()
